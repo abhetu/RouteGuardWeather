f61165956488ade4102e183e31f0b05a748890cf
Anubhav Bhetuwal <anubhavbhetuwal42@gmail.com>
2025-10-27 17:17:39 -0500

    Initial Commit

---
+++ RouteGuardWeather/RouteGuardWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteGuardWeatherApp.swift
+//  RouteGuardWeather
+//
+//  Created by Anubhav Bhetu on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct RouteGuardWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
